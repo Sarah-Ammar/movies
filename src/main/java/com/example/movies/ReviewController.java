8fd59413a3b18dad608e3afa856503adfc2c3ad3
Sarah-Ammar <sarah.ammar94.sa90@gmail.com>
2025-05-04 00:23:53 +0200

    Backend implemnted with movies dataset

---
+++ src/main/java/com/example/movies/ReviewController.java
@@ -0,0 +1,21 @@
+package com.example.movies;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/reviews")
+public class ReviewController {
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
+        return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")),
+                HttpStatus.CREATED);
+
+    }
+}
